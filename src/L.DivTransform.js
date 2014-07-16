4368efed5e07c1503c5081f97461d1f413280e99
OriginalSin <saleks@scanex.ru>
2014-07-16 10:17:43 +0400

    Extent from L.ImageTransform

--- src/L.DivTransform.js
+++ src/L.DivTransform.js
@@ -1,31 +1,18 @@
-L.DivTransform = L.ImageOverlay.extend({
+L.DivTransform = L.ImageTransform.extend({
     initialize: function (url, anchors, options) { // (String, LatLngBounds, Object)
         L.ImageOverlay.prototype.initialize.call(this, url, anchors, options);
         this.setAnchors(anchors);
     },
 
-    setAnchors: function (anchors) {
-        this._anchors = [];
-        this._bounds = L.latLngBounds(anchors);
-        for (var i = 0, len = anchors.length; i < len; i++) {
-            var yx = anchors[i];
-            this._anchors.push(L.latLng(yx));
-        }
-        
-        if (this._map) {
-            this._reset();
-        }
-    },
-
     _initImage: function () {
         this._image = L.DomUtil.create('div', 'leaflet-image-layer');
-        this._image.style.pointerEvents = 'none';
+        //this._image.style.pointerEvents = 'none';
 
-            // if (this._map.options.zoomAnimation && L.Browser.any3d) {
-            // L.DomUtil.addClass(this._image, 'leaflet-zoom-animated');
-        // } else {
+            if (this._map.options.zoomAnimation && L.Browser.any3d) {
+            L.DomUtil.addClass(this._image, 'leaflet-zoom-animated');
+        } else {
             L.DomUtil.addClass(this._image, 'leaflet-zoom-hide');
-        // }
+        }
 
         this._imgNode = L.DomUtil.create('div');
         this._imgNode.style[L.DomUtil.TRANSFORM_ORIGIN] = '0 0';
@@ -45,6 +32,9 @@ L.DivTransform = L.ImageOverlay.extend({
             w = this.options.width, 
             h = this.options.height,
             pixels = [];
+
+        div.style.width  = size.x + 'px';
+        div.style.height = size.y + 'px';
 
         for (var i = 0, len = anchors.length; i < len; i++) {
             var p = map.latLngToLayerPoint(anchors[i]);
@@ -66,15 +56,6 @@ L.DivTransform = L.ImageOverlay.extend({
         this._matrix3d_inverse = L.ImageTransform.Utils.adj(matrix3d);
         
         imgNode.style[L.DomUtil.TRANSFORM] = this._getMatrix3dCSS(this._matrix3d);
-    },
-
-    _getMatrix3dCSS: function(arr)	{		// get CSS atribute matrix3d
-        var css = 'matrix3d(';
-        css += arr[0].toFixed(9) + "," + arr[3].toFixed(9) + ", 0," + arr[6].toFixed(9);
-        css += "," + arr[1].toFixed(9) + "," + arr[4].toFixed(9) + ", 0," + arr[7].toFixed(9);
-        css += ",0, 0, 1, 0";
-        css += "," + arr[2].toFixed(9) + "," + arr[5].toFixed(9) + ", 0, " + arr[8].toFixed(9) + ")";
-        return css;
     }
 });
 
